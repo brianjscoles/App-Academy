3c72552f2721f987af33336f807bcae8a5ea4f47
bscoles <brianscoles@gmail.com>
2014-07-02 22:58:57 -0700

    Update 01_no_repeats.rb

--- Coding Challenge practice problems/Challenge 2/01_no_repeats.rb
+++ Coding Challenge practice problems/Challenge 2/01_no_repeats.rb
@@ -1,8 +1,7 @@
 def no_repeats(year_start, year_end)
 	
 	def all_uniq?(num)
-		return false if num.to_s.split("").uniq.length < num.to_s.length
-		return true
+	  return num.to_s.split("").uniq.length == num.to_s.length
 	end
 
 	return (year_start..year_end).select{|year| all_uniq?(year)}
